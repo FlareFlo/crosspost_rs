264ba499abc64d53ee8709c6bd2f00fe417b727f
FlareFlo <florianlipp17@gmail.com>
2022-02-15 19:26:03 +0100

    debug

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,7 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
+	println!("Loaded token: {}", TOKEN);
 	let mut client =
 		Client::builder(&TOKEN).event_handler(Handler).await.expect("Err creating client");
 
