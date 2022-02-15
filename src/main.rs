a47c8c959d928ea3551fce982f1a87036a91b3ec
FlareFlo <florianlipp17@gmail.com>
2022-02-15 16:51:10 +0100

    init-commit

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use serenity::{
+	async_trait,
+	model::{channel::Message, gateway::Ready},
+	prelude::*,
+};
+
+const DOMAIN: &str = "warthunder.com";
+const WHITELIST: &str = include_str!("../assets/whitelist.txt");
+const TOKEN: &str = include_str!("../assets/token.txt");
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+	async fn message(&self, ctx: Context, msg: Message) {
+		if WHITELIST.contains(&msg.channel_id.0.to_string()) {
+			if msg.content.contains(DOMAIN) {
+				match msg.crosspost(ctx).await {
+					Ok(_) => {
+						println!("Cross-posted message in {}", msg.channel_id.0);
+					}
+					Err(e) => {
+						eprintln!("Failed to cross post because: {}", e);
+					}
+				}
+			} else {
+                println!("Message does not contain the domain required");
+            }
+		} else {
+            println!("Message is not part of watched channels");
+        }
+	}
+
+	async fn ready(&self, _: Context, ready: Ready) {
+		println!("{} is connected!", ready.user.name);
+	}
+}
+
+#[tokio::main]
+async fn main() {
+	let mut client =
+		Client::builder(&TOKEN).event_handler(Handler).await.expect("Err creating client");
+
+	if let Err(why) = client.start().await {
+		println!("Client error: {:?}", why);
+	}
+}
