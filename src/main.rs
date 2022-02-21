7a982cc0d3457d9fca59dffb4ce9e707fbd97a4e
FlareFlo <florianlipp17@gmail.com>
2022-02-21 16:46:36 +0100

    More DB

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
+use std::cell::UnsafeCell;
 use std::str::FromStr;
-use serenity::{
-	async_trait,
-	model::{channel::Message, gateway::Ready},
-	prelude::*,
-};
-use sqlx::{ConnectOptions};
+use std::sync::MutexGuard;
+use serenity::{async_trait, Client, model::{channel::Message, gateway::Ready}};
+use serenity::prelude::{Context, EventHandler};
+use sqlx::{ConnectOptions, SqliteConnection};
 use sqlx::sqlite::SqliteJournalMode;
+use tokio::sync::Mutex;
 
 const TOKEN: &str = include_str!("../assets/token.txt");
 
-struct Handler;
+struct Handler {
+	db: Mutex<SqliteConnection>,
+}
 
 #[async_trait]
 impl EventHandler for Handler {
 	async fn message(&self, ctx: Context, msg: Message) {
 			println!("{}", msg.content);
+
+
+	// 	let yes = sqlx::query(
+	// 		r#"
+	// 	INSERT INTO messages (author, date_received)
+	// 	VALUES (?, ?);
+	// "#
+	// 	).bind(1)
+	// 		.bind(2)
+	// 		.execute(self.db)
+	// 		.await.unwrap();
 	}
 
 	async fn ready(&self, _: Context, ready: Ready) {
@@ -30,19 +43,14 @@ async fn main() {
 		.journal_mode(SqliteJournalMode::Wal)
 		.connect().await.unwrap();
 
-	let query = sqlx::query!(
-		r#"
-		INSERT INTO messages (author, date_received)
-		VALUES (?, ?);
-	"#,
-	0_i32 ,0_i32
-	);
+	let token = &TOKEN.to_string().replace("\n", "");
 
-	query.fetch_one(&mut db).await.unwrap();
-	// let token = &TOKEN.to_string().replace("\n", "");
-	// let mut client = Client::builder(token).event_handler(Handler).await.expect("Err creating client");
-	//
-	// if let Err(why) = client.start().await {
-	// 	println!("Client error: {:?}", why);
-	// }
+	let handler = Handler {
+		db: Mutex::new(db)
+	};
+	let mut client = Client::builder(token).event_handler(handler).await.expect("Err creating client");
+
+	if let Err(why) = client.start().await {
+		println!("Client error: {:?}", why);
+	}
 }
