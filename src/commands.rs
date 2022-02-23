974dc065c4ea728ed90437f97a598acc223c19f8
flareflo <florianlipp17@gmail.com>
2022-02-23 12:39:23 +0100

    mORE POISE

--- src/commands.rs
+++ src/commands.rs
@@ -60,7 +60,7 @@ pub async fn explode(
 /// Run with no arguments to register in guild, run with argument "global" to register globally.
 #[poise::command(prefix_command, hide_in_help)]
 pub async fn register(ctx: Context<'_>, #[flag] global: bool) -> Result<(), Error> {
-	poise::builtins::register_application_commands(ctx, false).await?;
+	poise::builtins::register_application_commands(ctx, global).await?;
 
 	Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 mod handler;
 mod commands;
 
+use std::path::Path;
 use std::str::FromStr;
 use std::sync::{Arc};
-use serenity::{Client};
-use serenity::client::bridge::gateway::ShardManager;
-use serenity::framework::StandardFramework;
-use serenity::prelude::TypeMapKey;
+use poise::builtins::register_application_commands;
 use sqlx::{ConnectOptions};
 use sqlx::sqlite::SqliteJournalMode;
 use tokio::sync::Mutex;
@@ -19,26 +17,27 @@ const TOKEN: &str = include_str!("../assets/token.txt");
 async fn main() {
 	dotenv::dotenv().expect("Failed to read .env file");
 
-	let db = sqlx::sqlite::SqliteConnectOptions::from_str(&std::env::var("DATABASE_URL").unwrap()).unwrap()
+	// Fun with the path, as the path fn properly escapes paths for windows or linux
+	let env = std::env::var("DATABASE_URL").unwrap().to_owned();
+	let path = Path::new(&env);
+
+	let db = sqlx::sqlite::SqliteConnectOptions::from_str(path.to_str().unwrap()).unwrap()
 		.journal_mode(SqliteJournalMode::Wal)
 		.connect().await.unwrap();
 
-	let token = &TOKEN.to_string().replace("\n", "");
-
-	let handler = Handler {
-	};
-
-	poise::Framework::build()
-		.token(include_str!("../assets/token.txt"))
-		.user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(()) }))
-		.options(poise::FrameworkOptions {
-			// configure framework here
-			prefix_options: poise::PrefixFrameworkOptions {
-				prefix: Some("!".into()),
+		poise::Framework::build()
+			.token(include_str!("../assets/token.txt"))
+			.user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move {
+				register_application_commands(_ctx, true).await.unwrap();
+				Ok(())
+			}))
+			.options(poise::FrameworkOptions {
+				prefix_options: poise::PrefixFrameworkOptions {
+					prefix: Some("!".into()),
+					..Default::default()
+				},
+				commands: vec![register(), explode()],
 				..Default::default()
-			},
-			commands: vec![age(), register(), explode()],
-			..Default::default()
-		})
-		.run().await.unwrap();
+			})
+			.run().await.unwrap();
 }
