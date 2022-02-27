89266682bc328ecf584ce55eed559d4600c1c215
FlareFlo <florianlipp17@gmail.com>
2022-02-27 01:49:02 +0100

    Added DB to message event

--- src/handler.rs
+++ src/handler.rs
@@ -1,4 +1,5 @@
 use poise::serenity_prelude::Context;
+use sqlx::{Row, Sqlite};
 use crate::Data;
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
@@ -7,14 +8,30 @@ pub async fn event_listener(
 	ctx: &Context,
 	event: &poise::Event<'_>,
 	_framework: &poise::Framework<Data, Error>,
-	_user_data: &Data,
+	user_data: &Data,
 ) -> Result<(), Error> {
 	match event {
 		poise::Event::Ready { data_about_bot } => {
 			println!("{} is connected!", data_about_bot.user.name)
 		}
 		poise::Event::Message { new_message } => {
-				new_message.crosspost(ctx).await.unwrap();
+			let mut db = user_data.db.lock().await;
+
+			let result = sqlx::query(
+				r#"
+						SELECT COUNT(*) AS count
+						FROM channels
+						WHERE id = ?;
+					"#
+			).bind(new_message.channel_id.0 as i64).fetch_one(&mut *db).await.unwrap();
+
+			match result.try_get("count") {
+				Ok(1) => {
+					new_message.crosspost(ctx).await;
+				}
+				_ => {
+				}
+			}
 		}
 		_ => {}
 	}
