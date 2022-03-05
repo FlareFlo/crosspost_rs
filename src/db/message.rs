4ba71e8354aa9b8d59b79409a82969ff0180ce8f
FlareFlo <mail@flareflo.dev>
2022-03-05 19:24:52 +0100

    Fixed query according to DB layout

--- src/db/message.rs
+++ src/db/message.rs
@@ -6,9 +6,14 @@ impl CrossDb {
 	pub async fn messages_log_message(&self, new_message: &Message) {
 		let _ = sqlx::query(
 			r#"
-				INSERT INTO messages (author, date_received)
-				VALUES (?, ?);
+				INSERT INTO messages (id, author, date_received, channel_id)
+				VALUES (?, ?, ?, ?);
 			"#
-		).bind(new_message.author.id.0 as i64).bind(new_message.timestamp.unix_timestamp()).execute(&self.db).await.unwrap();
+		).bind(new_message.id.0 as i64)
+			.bind(new_message.author.id.0 as i64)
+			.bind(new_message.timestamp.unix_timestamp())
+			.bind(new_message.channel_id.0 as i64)
+			.execute(&self.db)
+			.await.unwrap();
 	}
 }
