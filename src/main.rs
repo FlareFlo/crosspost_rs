01fd9306e871a1f5f676fbff3cb825f45441f834
FlareFlo <mail@flareflo.dev>
2025-04-05 02:09:45 +0200

    Remote content whitelisting due to lack of message intents

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use std::env;
 use serenity::{
-	async_trait,
-	model::{channel::Message, gateway::Ready},
-	prelude::*,
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
 };
+use std::env;
 use uptime_kuma_pusher::UptimePusher;
-
-const DOMAIN: &str = "warthunder.com";
 const WHITELIST: &str = include_str!("../assets/whitelist.txt");
 const TOKEN: &str = include_str!("../assets/token.txt");
 
@@ -14,42 +12,46 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-	async fn message(&self, ctx: Context, msg: Message) {
-		println!("Got message from: {} in {:?}", msg.author.name, msg.channel_id.name(&ctx).await);
-		if WHITELIST.contains(&msg.channel_id.get().to_string()) {
-			println!("Matched message with content {}", msg.content);
-			if msg.content.contains(DOMAIN) {
-				match msg.crosspost(&ctx).await {
-					Ok(_) => {
-						println!("Cross-posted message in {}", msg.channel_id.get());
-					}
-					Err(e) => {
-						eprintln!("Failed to cross post because: {}", e);
-					}
-				}
-			} else {
-                println!("Message does not contain the domain required: {}", msg.content);
+    async fn message(&self, ctx: Context, msg: Message) {
+        println!(
+            "Got message from: {} in {:?}",
+            msg.author.name,
+            msg.channel_id.name(&ctx).await
+        );
+        if WHITELIST.contains(&msg.channel_id.get().to_string()) {
+            match msg.crosspost(&ctx).await {
+                Ok(_) => {
+                    println!("Cross-posted message in {}", msg.channel_id.get());
+                }
+                Err(e) => {
+                    eprintln!("Failed to cross post because: {}", e);
+                }
             }
-		} else {
-            println!("Message is not part of watched channels: {}", msg.channel_id.get());
+        } else {
+            println!(
+                "Message is not part of watched channels: {}",
+                msg.channel_id.get()
+            );
         }
-	}
+    }
 
-	async fn ready(&self, _: Context, ready: Ready) {
-		println!("{} is connected!", ready.user.name);
-	}
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
 }
 
 // Use current thread due to: https://github.com/mozilla/sccache/issues/1972
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-	UptimePusher::new(&env::var("UPTIME_URL").unwrap(), false).spawn_background();
-	println!("{}", "Spawned uptime pusher");
-	let token = &TOKEN.to_string().replace("\n", "");
-	let mut client =
-		Client::builder(token, GatewayIntents::non_privileged()).event_handler(Handler).await.expect("Err creating client");
+    UptimePusher::new(&env::var("UPTIME_URL").unwrap(), false).spawn_background();
+    println!("{}", "Spawned uptime pusher");
+    let token = &TOKEN.to_string().replace("\n", "");
+    let mut client = Client::builder(token, GatewayIntents::non_privileged())
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
 
-	if let Err(why) = client.start().await {
-		println!("Client error: {:?}", why);
-	}
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
